eca39ad67b792b705a885cd63d9f50fbf5887ce3
Yahya Parvar <yahyaparvar1@gmail.com>
2024-04-12 03:18:35 +0330

    fixed some styles

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -5,7 +5,5 @@ interface SectionContainerProps {
 }
 
 export default function SectionContainer({ children }: SectionContainerProps) {
-  return (
-    <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</section>
-  )
+  return <section className="mx-auto max-w-screen-2xl px-4">{children}</section>
 }
