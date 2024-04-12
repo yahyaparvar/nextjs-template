378100304e9d4bbe9b29f21ca0ac76ada252e497
Yahya Parvar <yahyaparvar1@gmail.com>
2024-04-12 14:46:43 +0330

    finished the template

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,28 +18,10 @@ export default function Footer() {
         <div className="mt-16 flex flex-col items-center">
           <div className="mb-3 flex space-x-4">
             <div className="flex items-center">
-              {siteMetadata.formspree === false ? (
-                <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
-              ) : (
-                <button className="flex items-center focus:outline-none">
-                  <SocialIcon kind="mail" size={6} />
-                </button>
-              )}
-            </div>
-            <div className="flex items-center">
               <SocialIcon kind="github" href={siteMetadata.github} size={6} />
             </div>
             <div className="flex items-center">
-              <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-            </div>
-            <div className="flex items-center">
-              <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
-            </div>
-            <div className="flex items-center">
               <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-            </div>
-            <div className="flex items-center">
-              <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
             </div>
           </div>
           <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -5,5 +5,5 @@ interface SectionContainerProps {
 }
 
 export default function SectionContainer({ children }: SectionContainerProps) {
-  return <section className="mx-auto max-w-screen-2xl px-4">{children}</section>
+  return <section className="mx-auto max-w-screen-xl px-4">{children}</section>
 }
