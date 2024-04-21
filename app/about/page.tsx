db0dac4e23688cb5abb3af7e0dee3ffe2e6531f3
Yahya Parvar <yahyaparvar1@gmail.com>
2024-04-22 02:25:32 +0330

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        About
+      </div>
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next Te,plate",
+  title: "Next Template",
   description: "`create-next-app` Template",
 };
 
