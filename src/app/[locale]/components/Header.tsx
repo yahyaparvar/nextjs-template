5b500bbba7919777bcaaa29d7e9232c7bff35792
Yahya Parvar <yahyaparvar1@gmail.com>
2024-04-29 17:05:40 +0330

    fixed top bar

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -13,14 +13,14 @@ export const Header: FC<Props> = ({ locale }) => {
   const t = useTranslations('')
   return (
     <div className='mx-auto flex max-w-screen-2xl flex-row items-center justify-between p-5'>
-      <a href='/'>
+      <Link lang={locale} href='/'>
         <div className='flex flex-row items-center'>
           <div className='mb-2 h-14 w-14'>
             <LogoIcon />
           </div>
           <strong className='mx-2 select-none'>Template</strong>
         </div>
-      </a>
+      </Link>
       <div className='flex flex-row items-center gap-3'>
         <nav className='mr-10 inline-flex gap-5'>
           <Link lang={locale} href={`/about`}>
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -62,7 +62,17 @@ export default function RootLayout({
             locale={locale}
             messages={messages as AbstractIntlMessages}
           >
-            <NextTopLoader color='var(--primary)' showSpinner={false} />
+            <NextTopLoader
+              initialPosition={0.08}
+              crawlSpeed={200}
+              height={3}
+              crawl={true}
+              easing='ease'
+              speed={200}
+              shadow='0 0 10px #2299DD,0 0 5px #2299DD'
+              color='var(--primary)'
+              showSpinner={false}
+            />
             <Header locale={locale} />
             <main className='mx-auto max-w-screen-2xl'>{children}</main>
           </NextIntlClientProvider>
