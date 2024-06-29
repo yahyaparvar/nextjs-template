8c0381d20f50e431b574293bdf2b502cc6850aed
Kevin <kevinluong1123@gmail.com>
2024-06-30 05:24:28 +1000

    finished fundariser initiatives

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,6 +4,8 @@ import { FC } from 'react'
 import GithubIcon from '../icons/github'
 import LogoIcon from '../icons/logo'
 import ThemeSwitch from './ThemeSwitch'
+import Button from '../components/Button'
+import { Initiatives } from '../components/Initiatives'
 interface Props {
   locale: string
 }
@@ -26,6 +28,8 @@ export const Header: FC<Props> = ({ locale }) => {
           <a href=''>Support</a>
           <a href=''>Other</a>
         </nav> */}
+        {/* <Button className='bg-yellow-700 ml-10 mr-5'>Inititives</Button> */}
+        <Initiatives/>
         <ThemeSwitch />
         <a
           href='https://github.com/yahyaparvar/nextjs-template'
