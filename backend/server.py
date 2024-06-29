a789aac62168c96d2f0a24cecd7cafbe079c10be
RichardZYang <richardyang2468@gmail.com>
2024-06-29 16:35:05 +1000

    basic clan and user functionality added

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from pocketbase import PocketBase
 from user import create_user
+from clan import create_clan
 # import PocketBase from 'pocketbase';
 
 app = Flask(__name__)
@@ -19,6 +20,7 @@ def api_hello():
 
 @app.route("/api/createUser", methods=["POST"])
 def create_user_route():
+    username = 'blasd'
     email = 'abcd'
     password = 'blah@gmail.com'
     
@@ -28,15 +30,28 @@ def create_user_route():
     response, status_code = create_user(username, email)
     return jsonify(response), status_code
 
-# @app.route('clans/add_member', methods=['POST'])
-# def add_member_to_clan():
-#     data = request.get_json()
-#     member_id = data['member_id']
-#     clan_id = data['clan_id']
+@app.route("/api/createClan", methods=["POST"])
+def create_user_route():
+    email = 'abcd'
+    password = 'blah@gmail.com'
+    
+    if not username or not email:
+        return jsonify({'error': 'Missing username or email'}), 400
 
-#     sendNewMember(member_id, clan_id)
+    response, status_code = create_user(username, email)
+    return jsonify(response), status_code
 
-#     return jsonify({'message': 'New member added to clan successfully'})
+@app.route('clans/add_member', methods=['POST'])
+def add_member_to_clan():
+    data = request.get_json()
+    userName = data['username']
+    clanName = data['clanname']
+
+    if not userName or not clanName:
+        return jsonify({'error': 'Missing username or clan name'}), 400
+
+    response, status_code = add_member_to_clan(userName, clanName)
+    return jsonify(response), status_code
 
 if __name__ == "__main__":
     app.run(debug=True)
