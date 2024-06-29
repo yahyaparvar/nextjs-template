c317d573109126f4fbae1f8372eddb6705db80bf
Calvin <z5413994@ad.unsw.edu.au>
2024-06-29 16:43:22 +1000

    login pag

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import LoginForm from "../components/LoginForm"
+
+export default function LoginPage() {
+    return (
+        <main className="flex items-center justify-center md:h-screen">
+            <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+                <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+                    <div className="w-32 text-white md:w-36">
+                        Logo Here
+                    </div>
+                </div>
+                <LoginForm />
+            </div>
+        </main>
+    )
+}
