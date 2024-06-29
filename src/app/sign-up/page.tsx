8be289fba93a76cac1c00c124089779ce5fe1206
Calvin <z5413994@ad.unsw.edu.au>
2024-06-29 22:12:33 +1000

    form is submittable

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -13,6 +13,7 @@ export default function SignUpPage() {
 
 
     const handleSubmit = async (e: FormEvent) => {
+        console.log("sign up form submit")
         e.preventDefault();
         if (password !== confirmPassword) {
             alert('Passwords do not match');
@@ -53,7 +54,7 @@ export default function SignUpPage() {
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Create an account
                         </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                 <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" />
